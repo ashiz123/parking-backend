d305dbf1ffbbd515adb6c5618b51507fec6245f7
ashiz123 <37142670+ashiz123@users.noreply.github.com>
2025-01-30 20:44:35 +0000

    edited database_config.js

--- myapp/config/database_config.js
+++ myapp/config/database_config.js
@@ -11,5 +11,14 @@ module.exports = {
         dvla : process.env.DVLA_API_KEY
         
     },
+    production: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        dialect: process.env.DIALECT,  
+        dvla : process.env.DVLA_API_KEY
+        
+    },
     // Add configurations for other environments (test, production) as needed
 };
